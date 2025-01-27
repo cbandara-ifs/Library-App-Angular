c00a4b28737bd9638dd1e58b8647e8248a5b7500
cbandara-ifs <chinthaka.sci.se@gmail.com>
2025-01-28 08:00:25 +1100

    http service unit tests

--- src/app/services/api-http.service.spec.ts
+++ src/app/services/api-http.service.spec.ts
@@ -1,16 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ApiHttpService } from "./api-http.service";
+import { HttpClient } from "@angular/common/http";
+import { of } from "rxjs";
 
-import { ApiHttpService } from './api-http.service';
-
-describe('ApiHttpService', () => {
+describe("ApiHttpService", () => {
   let service: ApiHttpService;
+  let mockHttpClient: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiHttpService);
+    mockHttpClient = jasmine.createSpyObj<HttpClient>("HttpClient", [
+      "get",
+      "post",
+    ]);
+
+    service = new ApiHttpService(mockHttpClient);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should create a get request", () => {
+    const mockUrl = "api/cart";
+
+    mockHttpClient.get.and.returnValue(of(null));
+
+    service.get(mockUrl).subscribe(() => {});
+
+    expect(mockHttpClient.get).toHaveBeenCalledWith(mockUrl);
+  });
+
+  it("should create a post request", () => {
+    const mockUrl = "api/cart";
+
+    mockHttpClient.post.and.returnValue(of(null));
+
+    service.post(mockUrl, {}).subscribe(() => {});
+
+    expect(mockHttpClient.post).toHaveBeenCalledWith(mockUrl, {});
   });
 });
