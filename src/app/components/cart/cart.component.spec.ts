8bc9f316f7baff5408a38537ae61062df54ae422
cbandara-ifs <chinthaka.sci.se@gmail.com>
2025-01-29 21:05:16 +1100

    Adding unit test for cart component

--- src/app/components/cart/cart.component.spec.ts
+++ src/app/components/cart/cart.component.spec.ts
@@ -1,21 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CartComponent } from './cart.component';
+import { CartComponent } from "./cart.component";
+import { CartService } from "src/app/services/cart.service";
+import { IBook } from "src/app/models/book.model";
+import { of } from "rxjs";
 
-describe('CartComponent', () => {
+describe("CartComponent", () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+  let mockCartService: jasmine.SpyObj<CartService>;
 
   beforeEach(() => {
+    mockCartService = jasmine.createSpyObj<CartService>("CartService", [
+      "getCart",
+      "remove",
+    ]);
+
+    const mockBooks: IBook[] = [
+      {
+        id: 2,
+        title: "React: The Big Picture",
+        slug: "react-big-picture",
+        authorId: 3,
+        category: "JavaScript",
+      },
+    ];
+
+    mockCartService.getCart.and.returnValue(of(mockBooks));
+
     TestBed.configureTestingModule({
-      declarations: [CartComponent]
+      declarations: [CartComponent],
+      providers: [{ provide: CartService, useValue: mockCartService }],
     });
+
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
